77d4aa226051b78104b9cbad2bd301a4f3ed54d3
unknown <mfjkd7795@gmail.com>
2021-07-12 22:28:53 -0700

    Added : Use console to add details to address book

--- src/AddressBookMainJava/AddressBookMain.java
+++ src/AddressBookMainJava/AddressBookMain.java
@@ -1,65 +1,50 @@
 package AddressBookMainJava;
-
+import java.util.*;
+import java.util.Scanner;
 
 /**
- * @author FATHA
+ * @author FATHA 
  * Purpose - To create an address book management system
  */
-class contact {
 
-	 private String firstname, lastname, address, city, state, email;
-	 private int zip, phonenumber;
+public class AddressBookMain {
 
-		// TODO Auto-generated constructor stub
+	public void AddDetails() {
+
+		ArrayList<Contacts> Book = new ArrayList<>();
 		
-		public void setStringDetails(){
-			
-			this.firstname = firstname;
-			this.lastname = lastname;
-			this.address = address;
-	        this.city = city;
-	        this.state = state;
-	        this.email = email;
-	        this.zip = zip;
-	        this.phonenumber = phonenumber;
-	        
-		}
+		Scanner sc = new Scanner(System.in);
+		Contacts details = new Contacts();
+		System.out.println("Enter the First Name of person");
+		details.setFirstname(sc.nextLine());
+		System.out.println("Enter the Last Name of person");
+		details.setLastname(sc.nextLine());
+		System.out.println("Enter the Address Name of person");
+		details.setAddress(sc.nextLine());
+		System.out.println("Enter the City Name of person");
+		details.setCity(sc.nextLine());
+		System.out.println("Enter the State Name of person");
+		details.setState(sc.nextLine());
+		System.out.println("Enter the Email of person");
+		details.setEmail(sc.nextLine());
+		System.out.println("Enter the Zip code of person");
+		details.setZip(sc.nextInt());
+		System.out.println("Enter the Phone number of person");
+		details.setNumber(sc.nextInt());
+		sc.close();
 		
-		public String getFirstname() {
-			return this.firstname = firstname;
-		}
+		Book.add(details);
 		
-		public String getLasttname() {
-			return this.lastname = lastname;
-		}
+		System.out.println(Book);
+	}
 
-		public String getAddress() {
-			return this.address = address;
-		}
-
-		public String getCity() {
-			return this.city = city;
-		}
-		public String getState() {
-			return this.state = state;
-		}
-		public String getEmail() {
-			return this.email = email;
-		}
-		public int getZip() {
-			return this.zip = zip;
-		}
-		public int getNumber() {
-			return this.phonenumber = phonenumber;
-		}
-}
-
-
-
-public class AddressBookMain{
 	public static void main(String[] args) {
-		
+
+		  
 		System.out.println("Welcome to Address Book Management System");
 		
+		AddressBookMain details = new AddressBookMain();
+		details.AddDetails();
+	
 	}
 }
---
+++ src/AddressBookMainJava/Contacts.java
@@ -0,0 +1,91 @@
+package AddressBookMainJava;
+
+class Contacts {
+
+	private String firstname, lastname, address, city, state, email;
+	private int zip, phonenumber;
+	
+	/*
+	 * Setters
+	 */
+	public void setFirstname(String firstname){
+
+		this.firstname = firstname;
+	}
+	
+	public void setLastname(String lastname){
+
+		this.lastname = lastname;
+	}
+	
+	public void setAddress(String address){
+
+		this.address = address;
+	}
+	
+	public void setCity(String city){
+
+		this.city = city;
+	}
+	
+	public void setState(String state){
+
+		this.state = state;
+	}
+	
+	public void setEmail(String email){
+
+		this.email = email;
+	}
+	
+	public void setZip(int zip){
+
+		this.zip = zip;
+	}
+	
+	public void setNumber(int phonenumber){
+
+		this.phonenumber = phonenumber;
+	}
+	
+	/*
+	 * Getters
+	 */
+
+	public String getFirstname() {
+		return this.firstname;
+	}
+
+	public String getLasttname() {
+		return this.lastname;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public String getCity() {
+		return this.city;
+	}
+
+	public String getState() {
+		return this.state;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public int getZip() {
+		return this.zip;
+	}
+
+	public int getNumber() {
+		return this.phonenumber;
+	}
+
+	public String toString() {
+		return "First name= " + firstname + " Last name= " + lastname + " Address= " + address + " City= " + city
+				+ " State= " + state + " Zip= " + zip + " Phone Number= " + phonenumber + " E-mail= " + email;
+	}
+}
